0614a7efd1d8a1917157578c56f9f88d8148332b
Mohammed Zubair HUSSAIN <mohammedzubair.hussain@amadeus.com>
2024-11-11 15:46:57 +0530

    first push to the repo

---
+++ venv/Scripts/Programs/FirstPgm_GenBandName.py
@@ -0,0 +1,5 @@
+
+input("Welcome to Band Name Generator.")
+cityName = input("What's the name of the city you grew up in?.")
+petName = input("What's yours pet's name?.")
+print("Your band name could be : "+cityName+" "+petName)
